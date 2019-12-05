e6d06d0526e8691dc1276c4749e146a93ce769c2
lsqyh1004 <1539429627@qq.com>
2019-12-05 11:31:49 +0800

    模糊

---
+++ src/main/java/com/qf/controller/ShangcController.java
@@ -0,0 +1,34 @@
+package com.qf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Controller
+public class ShangcController {
+    @RequestMapping("/shangc")
+    @ResponseBody
+    public String shangc(@RequestParam("file") MultipartFile file){
+        if (file.isEmpty()){
+            return "上传失败";
+        }
+        String fileName=file.getOriginalFilename();
+        String filepath="F:/img/";
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "").toUpperCase();
+        File dest=new File(filepath+uuid+fileName);
+        String s="http://10.12.159.76:1018/"+uuid+fileName;
+        try {
+            file.transferTo(dest);
+            return s;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "上传失败";
+    }
+}
